8168f6811f94a5477b83db234ef7e18dcc9acdc1
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-18 17:04:01 +0700

    update for file to push

---
+++ database/database.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-drive/mysql"
+)
+
+var DB*sql.DB
+
+func initDatabase(){
+	var err error
+	DB, err = sql.Open("mysql","localhost:root@tcp(localhost:3306)/cv_app")
+	if err!= nil{
+		log.Fatal("Connection to database failed:", err)
+	}
+	if err := DB.Ping(); err != nil {
+		log.Fatal("Timeout in database", err)
+	}
+
+	log.Println("Database Connection successful")
+}
