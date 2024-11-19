ed1a46d3604eb866e555a520294ab33eec5d0888
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-19 13:21:31 +0700

    update for main query

--- main.go
+++ main.go
@@ -19,25 +19,25 @@ func main() {
 	router.HandleFunc("/api/profile", handlers.CreateProfile).Methods("POST")
 	router.HandleFunc("/api/profile/{id}", handlers.UpdateProfile).Methods("PUT")
 
-	router.HandleFunc("/photo", handlers.GetPhoto).Methods("GET")
-	router.HandleFunc("/photo", handlers.UpdatePhoto).Methods("PUT")
-	router.HandleFunc("/photo", handlers.DeletePhoto).Methods("DELETE")
+	router.HandleFunc("/api/photo", handlers.GetPhoto).Methods("GET")
+	router.HandleFunc("/api/photo", handlers.UpdatePhoto).Methods("PUT")
+	router.HandleFunc("/api/photo", handlers.DeletePhoto).Methods("DELETE")
 
 	router.HandleFunc("/api/working-experience", handlers.GetExperience).Methods("GET")
 	router.HandleFunc("/api/working-experience/{id}", handlers.GetExperience).Methods("GET")
 	router.HandleFunc("/api/working-experience", handlers.UpdateExperience).Methods("PUT")
 
-	router.HandleFunc("/employment", handlers.GetEmployment).Methods("GET")
-	router.HandleFunc("/employment", handlers.CreateEmployment).Methods("POST")
-	router.HandleFunc("/employment/{id}", handlers.DeleteEmployment).Methods("DELETE")
+	router.HandleFunc("/api/employment", handlers.GetEmployment).Methods("GET")
+	router.HandleFunc("/api/employment", handlers.CreateEmployment).Methods("POST")
+	router.HandleFunc("/api/employment/{id}", handlers.DeleteEmployment).Methods("DELETE")
 
-	router.HandleFunc("/education", handlers.GetEducation).Methods("GET")
-	router.HandleFunc("/education", handlers.CreateEducation).Methods("POST")
-	router.HandleFunc("/education/{id}", handlers.DeleteEducation).Methods("DELETE")
+	router.HandleFunc("/api/education", handlers.GetEducation).Methods("GET")
+	router.HandleFunc("/api/education", handlers.CreateEducation).Methods("POST")
+	router.HandleFunc("/api/education/{id}", handlers.DeleteEducation).Methods("DELETE")
 
-	router.HandleFunc("/skill", handlers.GetSkills).Methods("GET")
-	router.HandleFunc("/skill", handlers.CreateSkill).Methods("POST")
-	router.HandleFunc("/skill/{id}", handlers.DeleteSkill).Methods("DELETE")
+	router.HandleFunc("/api/skill", handlers.GetSkills).Methods("GET")
+	router.HandleFunc("/api/skill", handlers.CreateSkill).Methods("POST")
+	router.HandleFunc("/api/skill/{id}", handlers.DeleteSkill).Methods("DELETE")
 
 	log.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
